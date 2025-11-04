c8f43ce1bd6fafe3441f6797879150792b19ca79
CORP\djordje.palavestra <djordje.palavestra@tesla-advisory.ba>
2025-11-04 16:01:53 +0100

    End of section 15

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T> ///where T : class -> ako hocemo da T bude referenti tip (npr. klasa) a ne value type (npr. int, bool)
+    {
+        public T Id { get; set; } = default!;
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Customer : Entity<CustomerId> // CustomerId is strongly typed identifier
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+        public static Customer Create(CustomerId id, string name, string email) // Rich domain 'Entity' implement Create static method
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentException.ThrowIfNullOrEmpty(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
+
+
+    }
+}
