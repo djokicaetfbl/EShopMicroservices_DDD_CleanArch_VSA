814c5e3a8db35734155a4db1190de866241ec552
CORP\djordje.palavestra <djordje.palavestra@tesla-advisory.ba>
2026-02-04 15:55:01 +0100

    presentation layer part 1

---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Web.Models.Ordering;
+
+namespace Shopping.Web.Services
+{
+    public interface IOrderingService
+    {
+        [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}")]
+        Task<GetOrdersResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
+
+        [Get("/ordering-service/orders/{ordername}")]
+        Task<GetOrdersByNameResponse> GetOrdersByName(string orderName);
+
+        [Get("/ordering-service/orders/customer/{customerId}")]
+        Task<GetOrdersBtCustomerResponse> GetOrdersByCustomer(Guid customerId);
+    }
+}
